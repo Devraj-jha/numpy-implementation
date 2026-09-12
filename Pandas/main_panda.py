c160ec0fe1b785b6af572065615233ba516a7858
Devraj-jha <devrajjha100806@gmail.com>
2026-09-12 19:30:02 +0530

    pandas

--- Pandas/main_panda.py
+++ Pandas/main_panda.py
@@ -1,15 +1,39 @@
-# import pandas as pd
+import pandas as pd
 
-# df = pd.read_csv("Pandas/student.csv")
+df = pd.read_csv("Pandas/student.csv")
+
+#seris = one column of data.
+#index based.
+
+print(df)
+
+ages = pd.Series([20,21,19,25])
+
+print(ages[0])
+
+## data frame  it is a table.
 
 
+dfF = pd.DataFrame({
+    "name": ["Alice", "Bob", "Charlie"],
+    "age": [20, 25, 22],
+    "city": ["Delhi", "Mumbai", "Delhi"]
+})
 
-# fibo nachi sereis..
+## with a proper table 
 
-# fibo 1 1 2 3 5 8
+print(dfF)
 
-def fibo(nn):
-    if nn == 1:
-        return 
-    return fibo(nn) + fibo(nn - 1)
-print(fibo(44))
+print(dfF.index)
+
+
+## differnt methods.
+# method of insptect the top of head.
+
+# method of inspect the bottom. 
+
+# method to inspect the shape.
+
+# method to get a column
+
+print(df.info())
