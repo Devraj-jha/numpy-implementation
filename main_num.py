9090d83d4965b99501e2f1d4baa6ffc3d51788c4
Devraj-jha <devrajjha100806@gmail.com>
2026-09-12 14:27:24 +0530

    enough for next step

---
+++ main_num.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+a  = np.array([1,2,3,4,5])
+
+print(a)
+
+matrix = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
+b = np.array([1, 2, 3])
+
+c = b * 2
+print(c)
+
+zero_2d = np.zeros((2,3))
+
+# there are differnt ways to create differnt type of arrays or whatever.
+
+
